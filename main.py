fce159c501372638b592dbf215c665a2c9a46783
Smartheena <smartheena83@gmail.com>
2021-01-06 14:25:02 +0000

    First commit: reply with quote

---
+++ main.py
@@ -0,0 +1,33 @@
+import discord
+import os
+import requests
+import json
+
+client = discord.Client()
+
+def get_quote():
+  response = requests.get("https://zenquotes.io/api/random")
+
+  json_data = json.loads(response.text)
+
+  quote = json_data[0]['q'] + " - " + json_data[0]['a']
+
+  return (quote)
+
+@client.event
+async def on_ready():
+  print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+
+  if message.content.startswith('$inspire'):
+    quote = get_quote()
+    await message.channel.send(quote)
+
+client.run(os.getenv('TOKEN'))
+
+
+
